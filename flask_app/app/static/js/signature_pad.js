06da54cd57ab78a1ffee874d3944cbc34c7b9c21
alannuarii <alannuariii@gmail.com>
2022-11-15 07:48:16 +0800

    Update input absen

---
+++ flask_app/app/static/js/signature_pad.js
@@ -0,0 +1,20 @@
+var canvas = document.getElementById("signature-pad");
+var ttd = document.getElementById("ttd");
+
+var signaturePad = new SignaturePad(canvas, {
+  backgroundColor: "rgb(255, 255, 255)", // necessary for saving image as JPEG; can be removed is only saving as PNG or SVG
+});
+
+document.getElementById("submit").addEventListener("click", function () {
+  if (signaturePad.isEmpty()) {
+    return alert("Please provide a signature first.");
+  }
+
+  var data = signaturePad.toDataURL("image/png");
+  ttd.value = data
+  console.log(ttd.value)
+});
+
+document.getElementById("clear").addEventListener("click", function () {
+  signaturePad.clear();
+});
