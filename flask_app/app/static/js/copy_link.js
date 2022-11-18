53220ede00f7b8c3ed9a43a47f272f3090109dd4
alannuarii <alannuariii@gmail.com>
2022-11-18 17:06:46 +0800

    Update agenda

--- flask_app/app/static/js/copy_link.js
+++ flask_app/app/static/js/copy_link.js
@@ -1,19 +1,49 @@
-const copyButton = document.querySelectorAll(".btn-copy");
-const copyText = document.querySelectorAll(".link");
-const checklist = document.querySelectorAll(".checklist span i");
-const closeButton = document.querySelectorAll(".btn-keluar");
+// const copyButton = document.querySelectorAll(".btn-copy");
+// const copyText = document.querySelectorAll(".link");
+// const checklist = document.querySelectorAll(".checklist span i");
+// const closeButton = document.querySelectorAll(".btn-keluar");
 
-for (let i = 0; i < copyButton.length; i++) {
-  copyButton[i].addEventListener("click", (e) => {
+const linkBtn = document.querySelectorAll(".bi-link");
+const copyLink = document.querySelectorAll(".copy-link");
+const btnUp = document.querySelectorAll(".btn-up");
+const btnCopy = document.querySelectorAll(".bi-files");
+const copyText = document.querySelectorAll(".link");
+
+// linkBtn.addEventListener("click", (e) => {
+//   console.log(e);
+// });
+
+for (let i = 0; i < linkBtn.length; i++) {
+  linkBtn[i].addEventListener("click", (e) => {
     e.preventDefault();
+    copyLink[i].style.display = "block";
+  });
+
+  btnCopy[i].addEventListener("click", (e) => {
     copyText[i].select();
     copyText[i].setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copyText[i].value);
-    checklist[i].style.visibility = "visible";
+    btnCopy[i].style.color = "#ffc451";
   });
 
-  closeButton[i].addEventListener("click", (e) => {
+  btnUp[i].addEventListener("click", (e) => {
     e.preventDefault();
-    checklist[i].style.visibility = "hidden";
+    copyLink[i].style.display = "none";
+    btnCopy[i].style.color = "#fff";
   });
 }
+
+// for (let i = 0; i < copyButton.length; i++) {
+//   copyButton[i].addEventListener("click", (e) => {
+//     e.preventDefault();
+// copyText[i].select();
+// copyText[i].setSelectionRange(0, 99999);
+// navigator.clipboard.writeText(copyText[i].value);
+//     checklist[i].style.visibility = "visible";
+//   });
+
+//   closeButton[i].addEventListener("click", (e) => {
+//     e.preventDefault();
+//     checklist[i].style.visibility = "hidden";
+//   });
+// }
