0d3dc6d34a1eda239a70886f5ca2321e06f8b5b4
alannuarii <alannuariii@gmail.com>
2022-11-28 15:55:36 +0800

    Update folder ttd

--- flask_app/config.py
+++ flask_app/config.py
@@ -4,5 +4,7 @@ class Config(object):
     FOTO_P3K = 'app/static/img/p3k/foto'
     FOTO_HYDRANT = 'app/static/img/hydrant/foto'
     FOTO_TTD = 'app/static/img/absen/ttd'
+    FOTO_APAR_TTD = 'app/static/img/apar/ttd'
+    FOTO_P3K_TTD = 'app/static/img/p3k/ttd'
     FOTO_HYDRANT_TTD = 'app/static/img/hydrant/ttd'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
