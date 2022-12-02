dcfd1db3f99a275c7efe01212dd21640e11231f8
alannuarii <alannuariii@gmail.com>
2022-12-02 11:37:42 +0800

    Update

--- flask_app/app/static/js/agenda_toggle.js
+++ flask_app/app/static/js/agenda_toggle.js
@@ -1,9 +1,12 @@
 const down = document.querySelector(".bi-chevron-double-down");
-
-// function agendaToggle() {
-//   down.classList.toggle("bi-chevron-double-right");
-// }
+const kondisiDown = document.querySelectorAll("#dashboard .bi-chevron-double-down");
 
 down.addEventListener("click", () => {
   down.classList.toggle("bi-chevron-double-up");
 });
+
+for (let i = 1; i < kondisiDown.length; i++) {
+  kondisiDown[i].addEventListener("click", () => {
+    kondisiDown[i].classList.toggle("bi-chevron-double-up");
+  });
+}
