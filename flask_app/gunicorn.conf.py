f6ff7e75f4b42a8ea71e9df329250a1650a2c8ae
alannuarii <alannuariii@gmail.com>
2023-01-01 20:51:27 +0800

    Update

--- flask_app/gunicorn.conf.py
+++ flask_app/gunicorn.conf.py
@@ -4,10 +4,10 @@ from os import environ
 def max_workers():
     return cpu_count()
 
-chdir = '/home/alanwebi/repositories/K3App/flask_app'
-bind = '127.0.0.1:52335'
-max_requests = 1000
-workers = max_workers()
+chdir = '/home/alanwebi/repositories/k3app.alanpy.xyz/flask_app'
+bind = '127.0.0.1:52782'
+max_requests = 100
+workers = 1
 worker_class = 'tornado'
 errorlog = 'error.log'
 loglevel = 'error'
